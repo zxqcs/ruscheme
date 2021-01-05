1a0b5a57b1937e9bf1255c6e1052cfb6e5ad753f
Yichangcs <yccsml@163.com>
2021-01-05 15:19:45 +0800

    lay the foundation for the interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 use std::fmt;
 
+mod core {
+/*
 struct Pair {
     lhs: Option<Exp>,
     rhs: Option<Exp>,
 }
+*/
 
+/*
 #[allow(dead_code)]
 impl Pair {
     fn car(&self) -> &Option<Exp> {
@@ -16,12 +20,25 @@ impl Pair {
     }
 } 
 
-fn pretty_print(x: &Option<Exp>) {}
+fn is_pair(x: &Exp) -> bool {
+   true 
+}
+
+fn pretty_print(x: &Option<Exp>) {
+    match &x {
+        None => println!("()"),
+        &Some(exp) => {
+            if is_pair(&exp) {
+                pretty_print(&x);
+            }      
+        }
+    }
+}
 
 impl fmt::Display for Pair {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let lhs = &self.lhs;
-        let rhs = &self.rhs;
+        let lhs = &self.car();
+        let rhs = &self.cdr();
 
         write!(f, "(")?;
         pretty_print(lhs);
@@ -30,13 +47,84 @@ impl fmt::Display for Pair {
         write!(f, ")")
     }
 }
+*/
+enum Pair {
+    Cons(i32, Box<Pair>),
+    Nil,
+}
 
-struct Exp {}
+pub enum Exp {
+    FloatNumber(f32),
+    Integer(i32),
+    List(Pair),
+    Symbol(&'static str),
+}
+
+#[allow(dead_code)]
+impl Exp {
+    fn is_pair(exp: &Exp) -> bool { true }
+
+    fn is_variable(exp: &Exp) -> bool { true }
+
+    fn is_quoted(exp: &Exp) -> bool { true }
+
+    fn is_tagged(exp: &Exp, tag: &'static str) -> bool { true }
+
+    fn is_assignment(exp: &Exp) -> bool { true }
+
+    fn is_definiton(exp: &Exp) -> bool { true }
+
+    fn is_symbol(exp: &Exp) -> bool { true }
+
+    fn is_lambda(exp: &Exp) -> bool { true }
+
+    fn is_if(exp: &Exp) -> bool { true }
+
+    fn is_begin(exp: &Exp) -> bool { true }
+
+    fn is_application(exp: &Exp) -> bool { true }
+
+    fn is_cond(exp: &Exp) -> bool { true }
+}
+/* operations on List variant  */
+fn car(exp: &Exp) -> Option<&Exp> {Some(exp)}
+
+fn cdr(exp: &Exp) -> Option<&Exp> {Some(exp)}
+
+fn cadr(exp: &Exp) -> Option<&Exp> {Some(exp)}
 
 struct Env {}
 
 struct Procedure {}
 
-fn eval(x: Exp, y: Env) -> Result<Exp, &'static str> {Ok(x)}
+fn eval(exp: Exp, env: Env) -> Result<Exp, &'static str> {Ok(exp)}
 
 fn apply(p: Procedure, args: Exp) -> Result<Exp, &'static str> {Ok(args)}
+}
+
+mod  represent{
+    use super::core::*; 
+    use std::any::Any;
+
+    fn is_number(exp: &dyn Any) -> bool { 
+        if exp.is::<i32>() || exp.is::<f64>() {
+            true
+        } else { false }
+    }
+
+    fn is_string(exp: &dyn Any) -> bool { 
+        if exp.is::<String>() {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn is_self_evaluating(x: &Exp) -> bool {
+        if is_number(x) {
+            true
+        } else if is_string(x) {
+            true
+        } else { false }
+    }
+} 
