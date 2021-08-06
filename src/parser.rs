d9456af5f880ee3b5ee8600412a55173b489e3e8
yccsml <yccsml@163.com>
2021-08-06 09:09:27 +0800

    fix a bug for parser

--- src/parser.rs
+++ src/parser.rs
@@ -24,7 +24,7 @@ pub mod parser {
                             break;
                         }
                     } else {
-                        break;
+                        continue;
                     }
                 }
                 Err(_e) => break,
